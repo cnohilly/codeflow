9404e6d5026280a45110b511fdfebaeec8f7fda0
ctnohilly <ctnohilly@gmail.com>
2022-10-05 21:36:27 -0400

    created mutations on front end and added apolloprovider

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    ...headers,
+    authorization: token ? `Bearer ${token}` : ''
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <p>New Application</p>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <p>New Application</p>
+      </div >
+    </ApolloProvider>
   );
 }
 
