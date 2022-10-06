055a475a65a5449e30e1a54ab1c4fcb2620522ea
ctnohilly <ctnohilly@gmail.com>
2022-10-05 21:23:21 -0400

    created users front end query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,13 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+
+    query users(){
+        users(){
+            _id
+            username
+            email
+        }
+    }
+
+`;
