e709a6343f52f6f313b63f877fff42afc1a2d187
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-10-06 18:29:24 -0400

    added SingleProject and Comment files

---
+++ client/src/components/Comment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+const Comment = () => {
+
+  return
+
+};
+
+export default Comment;
---
+++ client/src/pages/SingleProject.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Project from '../components/Project';
+
+const SingleProject = () => {
+
+  return
+
+};
+
+export default SingleProject;
