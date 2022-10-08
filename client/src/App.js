12b89acb67182aa7aedf2c2c55a86d2bdd23ab3b
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-10-08 13:42:36 -0400

    updated header for user-contextualized data

--- client/src/App.js
+++ client/src/App.js
@@ -22,11 +22,12 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
-
   return {
-    ...headers,
-    authorization: token ? `Bearer ${token}` : ''
-  }
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 const client = new ApolloClient({
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import Auth from '../../utils/auth'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Auth from "../../utils/auth";
 
 const Header = () => {
-
-  const logout = event => {
+  const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
@@ -13,36 +12,43 @@ const Header = () => {
   return (
     // section for Header component
     <header className="sticky-top">
-        <Navbar bg="black" variant="dark" expand="md" >
-          <Container fluid className="px-3">
-            
-            
-            {/* brand image and name */}
-            <Link to="/">
-              <Navbar.Brand>
-                <img
-                  src="/logo192.png"
-                  width="30"
-                  height="30"
-                  className="d-inline-block align-top"
-                  alt="React Bootstrap logo"
-                />{' '}
-                Codename-Poseidon
-              </Navbar.Brand>
-            </Link>  
-              {/* menu button */}
+      <Navbar bg="black" variant="dark" expand="md">
+        <Container fluid className="px-3">
+          {/* brand image and name */}
+          <Link to="/">
+            <Navbar.Brand>
+              <img
+                src="/logo192.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />{" "}
+              Codename-Poseidon
+            </Navbar.Brand>
+          </Link>
+          {/* menu button */}
 
-            
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <nav className="text-center ms-auto">
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Signup</Link>
-                <Link to="/">Profile</Link>
-              </nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <nav className="text-center ms-auto">
+              {Auth.loggedIn() ? (
+                <>
+                  <Link to="/">Profile</Link>
+                  <a href="/" onClick={logout}>
+                    Logout
+                  </a>
+                </>
+              ) : (
+                <>
+                  <Link to="/login">Login</Link>
+                  <Link to="/signup">Signup</Link>
+                </>
+              )}
+            </nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
