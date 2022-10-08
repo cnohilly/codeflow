3a4268246528a7d9263b17988dd1271403a64378
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-10-08 13:27:09 -0400

    updated comments and replies to display number of likes

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Card, Button, ButtonGroup } from 'react-bootstrap';
+import { Col, Card, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 
 const Comment = () => {
 
@@ -17,6 +17,7 @@ const Comment = () => {
                 style={{ width: "36px" }}
               />
             </div>
+            
             <div className="flex-grow-1">
               <div className="ms-3">
                 {/* comment user info */}
@@ -30,20 +31,29 @@ const Comment = () => {
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos a alias aliquam veritatis cupiditate dolore qui sit inventore possimus, natus odio molestiae illum quis officiis sed laborum labore saepe impedit.
                 </Card.Text>
               </div>
-              <ButtonGroup aria-label="Button group" className="mt-1 ms-1">
+
+              <ButtonToolbar aria-label="Toolbar with button groups" className="mt-1 ms-1">
                 {/* like button */}
-                <Button variant="link" type="button" className="link-info">
-                  <i className="bi bi-heart-fill"></i>
-                </Button>
-                {/* reply button */}
-                <Button variant="link" type="button" className="link-warning">
-                  <i className="bi bi-pencil-square"></i>
-                </Button>
-                {/* delete button */}
-                <Button variant="link" type="button" className="link-danger">
-                  <i className="bi bi-trash-fill"></i>
-                </Button>
-              </ButtonGroup>
+                <div className="d-flex align-items-center me-2">
+                  <Button variant="link" type="button" className="link-info pe-2">
+                    <i className="bi bi-heart-fill"></i>
+                  </Button>
+                  {/* number of likes */}
+                  <div>
+                    # of Likes
+                  </div>
+                </div>
+                <ButtonGroup aria-label="Button group">
+                  {/* reply button */}
+                  <Button variant="link" type="button" className="link-warning">
+                    <i className="bi bi-pencil-square"></i>
+                  </Button>
+                  {/* delete button */}
+                  <Button variant="link" type="button" className="link-danger">
+                    <i className="bi bi-trash-fill"></i>
+                  </Button>
+                </ButtonGroup>
+              </ButtonToolbar>
             </div>
           </div>
         </Card.Body>
