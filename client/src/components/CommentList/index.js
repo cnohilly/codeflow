b23f4de3ffc921eadfa17f0a13ae0120c0344afa
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-10-08 13:45:22 -0400

    added small edit to textarea ids

---
+++ client/src/components/CommentList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Col, Card, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
+
+const CommentList = () => {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default CommentList;
