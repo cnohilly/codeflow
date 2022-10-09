52576ccb838054f9e7fafd2c7808761558b080bb
ianzyvith <izyvith25@gmail.com>
2022-10-08 22:24:22 -0400

    minor syntax fix

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,42 +1,28 @@
 import { gql } from "@apollo/client";
 
 export const QUERY_USER = gql`
-
-query Query($input: UserSearchInput!) {
-  user(input: $input) {
-    _id
-    username
-    email
-    profileImage
-    friends {
+  query Query($input: UserSearchInput!) {
+    user(input: $input) {
       _id
       username
       email
-    }
-    posts {
-      _id
-      postBody
-      createdAt
-      createdBy {
-        _id
-        username
-      }
-      repoLink
-      deployedLink
-      replyCount
-    }
-  }
-}
-
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
+      profileImage
       friends {
         _id
         username
+        email
+      }
+      posts {
+        _id
+        postBody
+        createdAt
+        createdBy {
+          _id
+          username
+        }
+        repoLink
+        deployedLink
+        replyCount
       }
     }
   }
