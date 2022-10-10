d62e33c634f3fb42a1291b1a7ea89d0992880c78
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-10-10 17:10:29 -0400

    updated footer links

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,71 +3,132 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   // array of footer links for each column
-  // const columnLinks1 = [];
-  // const columnLinks2 = [];
-  // const columnLinks3 = [];
+  const companyLinks = [
+    {
+      name: "about",
+      text: "About"
+    },
+    {
+      name: "careers",
+      text: "Careers"
+    },
+    {
+      name: "partners",
+      text: "Partners"
+    },
+    {
+      name: "faq",
+      text: "FAQs"
+    },
+  ];
+  const socialMediaLinks = [
+    {
+      name: "twitter",
+      symbol: "bi bi-twitter",
+      text: "https://twitter.com/"
+    },
+    {
+      name: "facebook",
+      symbol: "bi bi-facebook",
+      text: "https://www.facebook.com"
+    },
+    {
+      name: "instagram",
+      symbol: "bi bi-instagram",
+      text: "https://www.instagram.com/"
+    },
+    {
+      name: "linkedin",
+      symbol: "bi bi-linkedin",
+      text: "https://www.linkedin.com/"
+    },
+    {
+      name: "stack-overflow",
+      symbol: "bi bi-stack-overflow",
+      text: "https://stackoverflow.com/"
+    },
+    {
+      name: "github",
+      symbol: "bi bi-github",
+      text: "https://github.com/"
+    }
+  ];
+  const contactLinks = [
+    {
+      name: "email",
+      symbol: "bi bi-envelope-fill",
+      text: "poseidon@gmail.com"
+    },
+    {
+      name: "phone",
+      symbol: "bi bi-telephone-fill",
+      text: "555-555-5555"
+    },
+  ];
 
   return (
     // section for Footer component
     <footer className="pt-5 pb-4 bg-black text-center text-md-start text-white">
       <Container>
-        <Row xs={1} md={5}>
+        <Row xs={1} md={4}>
           <Col>
             <h5 className="text-uppercase">About us</h5>
             <p>
             Made by Kyle Tang, Stephon Treadwell, Chris Nohilly, Braulio Loaiza, and Ian Zyvith
             </p>
           </Col>
-          <Col></Col>
+          {/* company */}
           <Col>
             <h5 className="text-uppercase">Company</h5>
-            <ul className="list-unstyled">
-              <li className="mb-2">
-                <a href="/" className="text-white">Link 1</a>
-              </li>
-              <li className="mb-2">
-                <a href="/" className="text-white">Link 2</a>
-              </li>
-              <li className="mb-2">
-                <a href="/" className="text-white">Link 3</a>
-              </li>
-              <li className="mb-2">
-                <a href="/" className="text-white">Link 4</a>
-              </li>
+            <ul className="list-unstyled d-flex flex-column">
+              {companyLinks.map(companyLink => (
+                <li className="mb-2">
+                  <a 
+                    key={companyLink.name}
+                    href="/"
+                    className="nav-link"
+                    onclick="return false;"
+                  >
+                    {companyLink.text}
+                  </a>
+                </li>
+              ))}
             </ul>
           </Col>
+          {/* follow us */}
           <Col>
             <h5 className="text-uppercase">Follow Us</h5>
-            <ul className="list-unstyled">
-              <li  className="mb-2">
-                <a href="/" className="text-white">Link 1</a>
-              </li>
-              <li  className="mb-2">
-                <a href="/" className="text-white">Link 2</a>
-              </li>
-              <li  className="mb-2">
-                <a href="/" className="text-white">Link 3</a>
-              </li>
-              <li  className="mb-2">
-                <a href="/" className="text-white">Link 4</a>
-              </li>
-            </ul>
+            <div className="mb-3">
+              {socialMediaLinks.map(socialMediaLink => (
+                <a 
+                  key={socialMediaLink.name}
+                  href={`${socialMediaLink.text}`} 
+                  className="btn link-light fs-2 me-3 py-0 px-0"
+                >
+                  <i className={`${socialMediaLink.symbol}`}></i>
+                </a>
+              ))}
+            </div>
           </Col>
+          {/* contact */}
           <Col>
             <h5 className="text-uppercase">Contact</h5>
-            <ul className="list-unstyled">
-              <li className="mb-2">
-                <a href="/" className="text-white">Link 1</a>
-              </li>
-              <li className="mb-2">
-                <a href="/" className="text-white">Link 2</a>
-              </li>
-              <li className="mb-2">
-                <a href="/" className="text-white">Link 3</a>
-              </li>
-              <li className="mb-2">
-                <a href="/" className="text-white">Link 4</a>
-              </li>
+            <ul className="list-unstyled d-flex flex-column">
+              {contactLinks.map(contactLink => (
+                <li className="mb-2 mx-auto mx-md-0">
+                  <a 
+                    key={contactLink.name}
+                    href="/" 
+                    className="nav-link d-flex"
+                  >
+                    <i className={`${contactLink.symbol} pe-2`}
+                    ></i>
+                    <div className="">
+                      {contactLink.text}
+                    </div>
+                  </a>
+                </li>
+              ))}
             </ul>
           </Col>
         </Row>
@@ -75,7 +136,7 @@ const Footer = () => {
         <div>
           <ul className="list-unstyled d-flex flex-column flex-md-row">
             <li className="mb-2 order-last order-md-first">
-              &copy; 2022 Project 3, Inc.
+              &copy; 2022 Poseidon, Inc. All Rights Reserved.
             </li>
             <li className="mb-2 mx-md-4">
               <a href="/" className="nav-link">Terms and Conditions</a>
