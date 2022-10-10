eea028822af49d4e4a19199ab6e4616b03f23b19
ianzyvith <izyvith25@gmail.com>
2022-10-10 17:35:57 -0400

    added error message when trying to access profile when not logged in

---
+++ client/src/components/ProfileError/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Col, Card, Row } from "react-bootstrap";
+
+const ProfileError = ({ username, joinDate }) => {
+  return (
+    <Col className="py-5">
+      {/* User Info card */}
+      <Card className="bg-dark bg-gradient text-white shadow">
+        <Card.Header className="bg-black bg-gradient text-white">
+          <Card.Title>Uh oh... There seems to be a problem 😰</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <p>
+            You need to be logged in to view this page. Please use the
+            navigation above to signup or login if you already have an account!
+          </p>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default ProfileError;
