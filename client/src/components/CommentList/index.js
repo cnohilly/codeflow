4e4f878c4d9539c9590154b1b574dd8e8bcba252
cnohilly <christopher.nohilly@snhu.edu>
2022-10-12 14:03:09 -0400

    set it so comment list doesnt render with an empty array

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Card, Row } from 'react-bootstrap';
 import Comment from '../Comment';
 
 const CommentList = (props) => {
@@ -9,9 +9,22 @@ const CommentList = (props) => {
     includeReplies = false
   } = props;
 
+  if (!comments) {
+    return (
+      <Row xs={1} className="g-3">
+        <Card className="dark-card-bg text-light shadow mb-3">
+          <Card.Body>
+            <Card.Text>
+              <h5>No comments to display.</h5>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Row>
+    )
+  }
+
   return (
     <Row xs={1} className="g-3">
-
       {comments.map(comment => {
         return <Comment key={comment._id} comment={comment} includeReplies={includeReplies} />
       })}
--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { Row } from "react-bootstrap";
+import { Card, Row } from "react-bootstrap";
 import Project from "../Project";
 
 const ProjectList = (props) => {
   const { projects } = props;
+console.log(projects);
+  if (!projects.length>0) {
+    return (
+      <Row xs={1} className="g-4">
+        <Card className="dark-card-bg text-light shadow mb-3">
+          <Card.Body>
+              <h5>No projects to display.</h5>
+          </Card.Body>
+        </Card>
+      </Row>
+    )
+  }
+
   return (
     <Row xs={1} className="g-4">
       {projects.map((project) => {
